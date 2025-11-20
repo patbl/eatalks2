5aeae6701f3ada40a89b2017ab374b60d82acc35
Patrick Brinich-Langlois <pbrinichlanglois@gmail.com>
2025-11-19 23:08:30 -0700

    Fix URLs

--- plugins/builders/rss_parser.rb
+++ plugins/builders/rss_parser.rb
@@ -25,34 +25,51 @@ class Builders::RssParser < SiteBuilder
         duration = item.itunes_duration
         duration_seconds = duration.respond_to?(:content) ? duration.content.to_i : duration.to_i
 
+        # Extract episode ID from GUID (e.g., "Buzzsprout-12217213" -> "12217213")
+        episode_id = item.guid&.content&.sub('Buzzsprout-', '')
+
+        # Extract full slug from audio URL
+        # e.g., "https://www.buzzsprout.com/1755269/episodes/12217213-hilary-greaves....mp3"
+        # becomes "12217213-hilary-greaves-..."
+        audio_url = item.enclosure&.url
+        slug = if audio_url && audio_url.include?('/episodes/')
+          audio_url.split('/episodes/').last&.sub(/\.mp3$/, '')
+        else
+          "#{episode_id}-#{slugify(item.title)}"
+        end
+
         {
           title: item.title,
           description: strip_html(item.description),
           summary: item.itunes_summary ? strip_html(item.itunes_summary) : strip_html(item.description),
-          audio_url: item.enclosure&.url,
+          audio_url: audio_url,
           duration: duration_seconds,
           pub_date: item.pubDate,
           guid: item.guid&.content,
+          episode_id: episode_id,
+          slug: slug,
           keywords: item.itunes_keywords
         }
       end
 
       site.data[:episodes] = episodes
 
-      # Create individual episode pages
+      # Create individual episode pages with Buzzsprout URL structure
       episodes.each_with_index do |episode, index|
-        slug = slugify(episode[:title])
-
         episode_data = episode.merge({
-          episode_number: episodes.length - index,
-          slug: slug
+          episode_number: episodes.length - index
         })
 
+        # Use the slug extracted from the RSS feed
+        slug = episode[:slug]
+
+        # Create path: /1755269/episodes/{slug}.html
         add_resource :episodes, "#{slug}.html" do
           layout :episode
           title episode_data[:title]
           content episode_data[:description]
           data episode_data
+          permalink "/1755269/episodes/#{slug}/"
         end
       end
     end
